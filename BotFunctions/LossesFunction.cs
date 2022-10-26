1819664ad9ab7ab7b4c8d290f990bfee87528ac2
eless <solovey.al@gmail.com>
2022-10-26 21:05:01 +0200

    add Losses function

---
+++ BotFunctions/LossesFunction.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BarracudaTestBot.Checkers;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+
+namespace BotFunctions
+{
+    public class LossesFunction
+    {
+        private readonly IConfiguration configuration;
+        private readonly WordChecker wordChecker;
+
+        public LossesFunction(IConfiguration configuration, WordChecker wordChecker)
+        {
+            this.configuration = configuration;
+            this.wordChecker = wordChecker;
+        }
+
+        [FunctionName("LossesFunction")]
+        public async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"LossesFunction executed at: {DateTime.Now}");
+
+            var token = configuration.GetValue<string>("TelegramToken");
+            var botClient = new TelegramBotClient(token);
+            if (wordChecker.GetAnswersByCommand("/losses").FirstOrDefault() is not { } message)
+            {
+                return;
+            }
+            await botClient.SendTextMessageAsync(
+                chatId: -1001344803304,
+                text: message.Text,
+                parseMode: message.ParseMode,
+                cancellationToken: CancellationToken.None);
+        }
+    }
+}
---
+++ BotFunctions/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BarracudaTestBot.Checkers;
+using BarracudaTestBot.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(FunctionApp.Startup))]
+
+namespace FunctionApp
+{
+    class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            string cs = Environment.GetEnvironmentVariable("TelegramToken");
+            builder.ConfigurationBuilder.AddEnvironmentVariables(cs);
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<WordChecker>();
+            builder.Services.AddSingleton<RussianLossesService>();
+        }
+    }
+}
