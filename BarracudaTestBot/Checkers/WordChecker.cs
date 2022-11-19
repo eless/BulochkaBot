8411576fb8a66ebeb5f53f509708e3461c84b138
eless <solovey.al@gmail.com>
2022-11-19 17:11:52 +0100

    change delegates to CommandAnswers

--- BarracudaTestBot/Checkers/WordChecker.cs
+++ BarracudaTestBot/Checkers/WordChecker.cs
@@ -13,20 +13,20 @@ public class WordChecker
     public WordChecker(RussianLossesService russianLossesService, PutinGenerator putinGenerator, StickerChecker stickerChecker) =>
         (_russianLossesService, _putinGenerator, _stickerChecker) = (russianLossesService, putinGenerator, stickerChecker);
 
-    protected Dictionary<Regex, Func<CommandAnswer>> Commands => new Dictionary<Regex, Func<CommandAnswer>>
+    protected Dictionary<Regex, CommandAnswer> Commands => new Dictionary<Regex, CommandAnswer>
     {
-        [new Regex("^Слава Україні!$")] = () => new CommandAnswer("*Героям слава\\!*", ParseMode.MarkdownV2),
-        [new Regex("шо по русні", RegexOptions.IgnoreCase)] = () => new CommandAnswer("*русні пизда\\!*", ParseMode.MarkdownV2),
-        [new Regex("путін", RegexOptions.IgnoreCase)] = () => new CommandAnswer(_putinGenerator.GenerateName(), ParseMode.Markdown),
-        [new Regex("булочка", RegexOptions.IgnoreCase)] = () => new CommandAnswer("мурняв", ParseMode.Markdown),
-        [new Regex($"^/losses")] = () => new CommandAnswer(_russianLossesService.GetData().Result, ParseMode.MarkdownV2),
-        [new Regex($"^/stickers")] = () => new CommandAnswer(string.Join(Environment.NewLine, _stickerChecker.GetCommands()), ParseMode.MarkdownV2),
+        [new Regex("^Слава Україні!$")] = new CommandAnswer("*Героям слава\\!*", ParseMode.MarkdownV2),
+        [new Regex("шо по русні", RegexOptions.IgnoreCase)] = new CommandAnswer("*русні пизда\\!*", ParseMode.MarkdownV2),
+        [new Regex("путін", RegexOptions.IgnoreCase)] = new CommandAnswer(_putinGenerator.GenerateName(), ParseMode.Markdown),
+        [new Regex("булочка", RegexOptions.IgnoreCase)] = new CommandAnswer("мурняв", ParseMode.Markdown),
+        [new Regex($"^/losses")] = new CommandAnswer(_russianLossesService.GetData().Result, ParseMode.MarkdownV2),
+        [new Regex($"^/stickers")] = new CommandAnswer(string.Join(Environment.NewLine, _stickerChecker.GetCommands()), ParseMode.MarkdownV2),
     };
 
     public IEnumerable<CommandAnswer> GetAnswersByCommand(string command)
     {
         var commands = Commands.Where(c => c.Key.IsMatch(command));
 
-        return commands.Select(c => c.Value.Invoke());
+        return commands.Select(c => c.Value);
     }
 }
