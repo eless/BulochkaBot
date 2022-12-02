7f978275927dec2e59736dda86fc58661848a4ca
eless <solovey.al@gmail.com>
2022-12-03 00:08:07 +0100

    added posibility to send stickers after wordChecker

--- BarracudaTestBot/Checkers/WordChecker.cs
+++ BarracudaTestBot/Checkers/WordChecker.cs
@@ -13,20 +13,27 @@ public class WordChecker
     public WordChecker(RussianLossesService russianLossesService, PutinGenerator putinGenerator, StickerChecker stickerChecker) =>
         (_russianLossesService, _putinGenerator, _stickerChecker) = (russianLossesService, putinGenerator, stickerChecker);
 
-    protected Dictionary<Regex, CommandAnswer> Commands => new Dictionary<Regex, CommandAnswer>
+    protected Dictionary<Regex, Action<List<CommandAnswer>>> Commands => new Dictionary<Regex, Action<List<CommandAnswer>>>
     {
-        [new Regex("^Слава Україні!$")] = new CommandAnswer("*Героям слава\\!*", ParseMode.MarkdownV2),
-        [new Regex("шо по русні", RegexOptions.IgnoreCase)] = new CommandAnswer("*русні пизда\\!*", ParseMode.MarkdownV2),
-        [new Regex("путін", RegexOptions.IgnoreCase)] = new CommandAnswer(_putinGenerator.GenerateName(), ParseMode.Markdown),
-        [new Regex("булочка", RegexOptions.IgnoreCase)] = new CommandAnswer("мурняв", ParseMode.Markdown),
-        [new Regex($"^/losses")] = new CommandAnswer(_russianLossesService.GetData().Result, ParseMode.MarkdownV2),
-        [new Regex($"^/stickers")] = new CommandAnswer(string.Join(Environment.NewLine, _stickerChecker.GetCommands()), ParseMode.MarkdownV2),
+        [new Regex("^Слава Україні!$")] = (commands) => commands.Add(new CommandAnswer("*Героям слава\\!*", ParseMode.MarkdownV2)),
+        [new Regex("шо по русні", RegexOptions.IgnoreCase)] = (commands) => commands.Add(new CommandAnswer("*русні пизда\\!*", ParseMode.MarkdownV2)),
+        [new Regex("путін", RegexOptions.IgnoreCase)] = (commands) => commands.Add(new CommandAnswer(_putinGenerator.GenerateName(), ParseMode.Markdown)),
+        [new Regex("булочка", RegexOptions.IgnoreCase)] = (commands) => commands.Add(new CommandAnswer("мурняв", ParseMode.Markdown)),
+        [new Regex($"^/losses")] = (commands) =>
+        {
+            commands.Add(new CommandAnswer(_russianLossesService.GetData().Result, ParseMode.MarkdownV2));
+            commands.Add(new CommandAnswer("русні пизда", ParseMode.Markdown));
+        },
+        [new Regex($"^/stickers")] = (commands) => commands.Add(new CommandAnswer(string.Join(Environment.NewLine, _stickerChecker.GetCommands()), ParseMode.MarkdownV2)),
     };
 
     public IEnumerable<CommandAnswer> GetAnswersByCommand(string command)
     {
-        var commands = Commands.Where(c => c.Key.IsMatch(command));
+        var commandsList = new List<CommandAnswer>();
+        Commands
+            .Where(c => c.Key.IsMatch(command))
+            .ToList().ForEach(action => action.Value(commandsList));
 
-        return commands.Select(c => c.Value);
+        return commandsList;
     }
 }
