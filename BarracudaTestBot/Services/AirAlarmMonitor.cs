4f89fd91355680f31a876c22595ca2a8f7e14969
OleksiiTeplyi <oleksiy.tepliy@developex.net>
2023-04-04 03:01:37 +0300

    add logs to an alarm monitor, commented timer for debug

--- BarracudaTestBot/Services/AirAlarmMonitor.cs
+++ BarracudaTestBot/Services/AirAlarmMonitor.cs
@@ -9,7 +9,7 @@ namespace BarracudaTestBot.Services
         private AirAlarmChecker _checker;
         private AirAlarmGenericNotifier _notifier;
         TelemetryClient _telemetry;
-        private readonly TimeSpan _alarmCheckPeriod = TimeSpan.FromSeconds(5);
+        //private readonly TimeSpan _alarmCheckPeriod = TimeSpan.FromSeconds(5);
 
         public AirAlarmMonitor(AirAlarmChecker checker, AirAlarmGenericNotifier notifier, TelemetryClient telemetry)
         {
@@ -20,21 +20,30 @@ namespace BarracudaTestBot.Services
 
         protected override async Task ExecuteAsync(CancellationToken cts)
         {
-            try
+            //using var timer = new PeriodicTimer(_alarmCheckPeriod);
+            while (!cts.IsCancellationRequested)
             {
-                using var timer = new PeriodicTimer(_alarmCheckPeriod);
-                while (!cts.IsCancellationRequested)
+                bool success = true;
+                try
                 {
-                    await timer.WaitForNextTickAsync();
+                    //await timer.WaitForNextTickAsync(); // TODO: temporary removed timer and add task delay
                     _telemetry.TrackEvent("ALERT POLLING");
-                    var result = _checker.Check().Result;
+                    var result = await _checker.Check();
                     _notifier.notify(result);
                     // TODO: maybe add report at midnight about all alerts this day, and wish a good night.
                 }
-            }
-            catch (Exception ex)
-            {
-                _telemetry.TrackException(ex);
+                catch (TaskCanceledException)
+                {
+                    _telemetry.TrackTrace("ALARM MONITOR CANCELLED!");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    _telemetry.TrackTrace("ALARM MONITOR FAILED");
+                    _telemetry.TrackException(ex);
+                    success = false;
+                }
+                await Task.Delay(success ? 5000 : 1000, cts);
             }
         }
     }
