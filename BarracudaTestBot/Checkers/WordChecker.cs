ba026d4a8a3e1b451fbbfd0fb9314546c29e7fd5
O <solovey.al@gmail.com>
2025-02-28 20:58:02 +0200

    add trump to our list of pidarily

--- BarracudaTestBot/Checkers/WordChecker.cs
+++ BarracudaTestBot/Checkers/WordChecker.cs
@@ -4,20 +4,19 @@ using Telegram.Bot.Types.Enums;
 
 namespace BarracudaTestBot.Checkers;
 
-public class WordChecker
+public class WordChecker(PutinGenerator pidorNamesGenerator, StickerChecker stickerChecker)
 {
-    private readonly StickerChecker _stickerChecker;
-    private PutinGenerator _putinGenerator;
-
-    public WordChecker(PutinGenerator putinGenerator, StickerChecker stickerChecker) =>
-        (_putinGenerator, _stickerChecker) = (putinGenerator, stickerChecker);
+    private readonly StickerChecker _stickerChecker = stickerChecker;
+    private PutinGenerator _putinGenerator = pidorNamesGenerator;
 
     protected Dictionary<Regex, Action<List<CommandAnswer>>> Commands => new()
     {
         [new Regex("^Слава Україні!$")] = (commands) => commands.Add(new CommandAnswer("*Героям слава\\!*", ParseMode.MarkdownV2)),
         [new Regex("шо по русні", RegexOptions.IgnoreCase)] = (commands) => commands.Add(new CommandAnswer("*русні пизда\\!*", ParseMode.MarkdownV2)),
-        [new Regex("путін", RegexOptions.IgnoreCase)] = (commands) => commands.Add(new CommandAnswer(_putinGenerator.GenerateName(), ParseMode.Markdown)),
-        [new Regex("маск", RegexOptions.IgnoreCase)] = (commands) => commands.Add(new CommandAnswer(_putinGenerator.GenerateName("маск"), ParseMode.Markdown)),
+        [new Regex("путін", RegexOptions.IgnoreCase)] = (commands) => commands.Add(new CommandAnswer(pidorNamesGenerator.GenerateName(), ParseMode.Markdown)),
+        [new Regex("трамп", RegexOptions.IgnoreCase)] = (commands) => commands.Add(new CommandAnswer("трамп вже поснідав гівном, просив добавку", ParseMode.Markdown)),
+        [new Regex("трамп", RegexOptions.IgnoreCase)] = (commands) => commands.Add(new CommandAnswer(pidorNamesGenerator.GenerateName("трамп"), ParseMode.Markdown)),
+        [new Regex("маск", RegexOptions.IgnoreCase)] = (commands) => commands.Add(new CommandAnswer(pidorNamesGenerator.GenerateName("маск"), ParseMode.Markdown)),
         [new Regex("булочка", RegexOptions.IgnoreCase)] = (commands) => commands.Add(new CommandAnswer("мурняв", ParseMode.Markdown)),
         [new Regex($"^/losses")] = (commands) => commands.Add(new CommandAnswer("losses", ParseMode.MarkdownV2)),
         [new Regex($"^/stickers")] = (commands) => commands.Add(new CommandAnswer(string.Join(Environment.NewLine, _stickerChecker.GetCommands()), ParseMode.MarkdownV2)),
