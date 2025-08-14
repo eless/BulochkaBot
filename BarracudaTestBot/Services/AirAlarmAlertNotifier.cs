7d465e90d212dc9b132cd1463c3a472ff33c667c
O <solovey.al@gmail.com>
2025-08-14 20:14:21 +0300

    fix build

--- BarracudaTestBot/Services/AirAlarmAlertNotifier.cs
+++ BarracudaTestBot/Services/AirAlarmAlertNotifier.cs
@@ -10,11 +10,19 @@ namespace BarracudaTestBot.Services
 
         public async void Send()
         {
-            await _botClient.SendStickerAsync(
-                chatId: -1001344803304,
-                sticker: InputFile.FromUri(_stickerSelector.GetAlertSticker()));
-            // TODO: add text yobana rusnia or get it from some generator 
-            // maybe add info about time between current alert and last all clear
+            try
+            {
+                await _botClient.SendStickerAsync(
+                    chatId: -1001344803304,
+                    sticker: InputFile.FromUri(_stickerSelector.GetAlertSticker()));
+                // TODO: add text yobana rusnia or get it from some generator 
+                // maybe add info about time between current alert and last all clear
+
+            } catch(Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
