2120f0ca1ae6a1845bfd296889591faf12235fca
O <solovey.al@gmail.com>
2025-09-22 01:38:28 +0300

    remove wrong using

--- BarracudaTestBot/Repositories/RussianLossesSubscriptionRepository.cs
+++ BarracudaTestBot/Repositories/RussianLossesSubscriptionRepository.cs
@@ -1,44 +1,42 @@
-﻿using System.Threading.Tasks;
-using BarracudaTestBot.Database;
+﻿using BarracudaTestBot.Database;
 using Microsoft.EntityFrameworkCore;
 
-namespace BarracudaTestBot.Repositories
+namespace BarracudaTestBot.Repositories;
+
+public class RussianLossesSubscriptionRepository(BotDbContext dbContext)
 {
-    public class RussianLossesSubscriptionRepository(BotDbContext dbContext)
+
+    public async Task Subscribe(RussianLossesSubscription subscription)
     {
-
-        public async Task Subscribe(RussianLossesSubscription subscription)
+        var existingSubscription = await dbContext.RussianLossesSubscriptions
+            .SingleOrDefaultAsync(entity => entity.ChatId == subscription.ChatId);
+        if (existingSubscription == default)
         {
-            var existingSubscription = await dbContext.RussianLossesSubscriptions
-                .SingleOrDefaultAsync(entity => entity.ChatId == subscription.ChatId);
-            if (existingSubscription == default)
-            {
-                await dbContext.RussianLossesSubscriptions.AddAsync(subscription);
-            }
-            else
-            {
-                existingSubscription.Hour = subscription.Hour;
-                existingSubscription.Minutes = subscription.Minutes;
-            }
-            await dbContext.SaveChangesAsync();
+            await dbContext.RussianLossesSubscriptions.AddAsync(subscription);
         }
-
-        public async Task Unsubscribe(long chatId)
+        else
         {
-            var subscription = await dbContext.RussianLossesSubscriptions
-                .FirstOrDefaultAsync(entity => entity.ChatId == chatId);
-            if (subscription != default)
-            {
-                dbContext.RussianLossesSubscriptions.Remove(subscription);
-            }
-            await dbContext.SaveChangesAsync();
+            existingSubscription.Hour = subscription.Hour;
+            existingSubscription.Minutes = subscription.Minutes;
         }
-
-        public async Task<RussianLossesSubscription?> GetLossesSubscription(long chatId) =>
-            await dbContext.RussianLossesSubscriptions
-                .FirstOrDefaultAsync(entity => entity.ChatId == chatId);
-
-        public IAsyncEnumerable<RussianLossesSubscription> GetAllLossesSubscriptions() =>
-            dbContext.RussianLossesSubscriptions.AsAsyncEnumerable();
+        await dbContext.SaveChangesAsync();
     }
+
+    public async Task Unsubscribe(long chatId)
+    {
+        var subscription = await dbContext.RussianLossesSubscriptions
+            .FirstOrDefaultAsync(entity => entity.ChatId == chatId);
+        if (subscription != default)
+        {
+            dbContext.RussianLossesSubscriptions.Remove(subscription);
+        }
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task<RussianLossesSubscription?> GetLossesSubscription(long chatId) =>
+        await dbContext.RussianLossesSubscriptions
+            .FirstOrDefaultAsync(entity => entity.ChatId == chatId);
+
+    public IAsyncEnumerable<RussianLossesSubscription> GetAllLossesSubscriptions() =>
+        dbContext.RussianLossesSubscriptions.AsAsyncEnumerable();
 }
